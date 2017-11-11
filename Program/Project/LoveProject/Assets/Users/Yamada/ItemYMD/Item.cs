ca727fe9180407a4ba3c39cbfc69664f5c5f014e
Issei <hal.nagoya40077@gmail.com>
2017-11-12 03:12:16 +0900

    アイテムの、機能・プレハブの追加_Yamada

--- Program/Project/LoveProject/Assets/Users/Yamada/ItemYMD/Item.cs
+++ Program/Project/LoveProject/Assets/Users/Yamada/ItemYMD/Item.cs
@@ -4,19 +4,46 @@ using UnityEngine;
 
 //	
 //	Item（お菓子）の抽象クラス
-public abstract class Item : MonoBehaviour {
-	
+public abstract class Item : MonoBehaviour
+{
 	//	メンバー
-	protected float breakHp;	//	耐久度
-	protected float attackPoint;	//	攻撃力・威力
+	[Header("耐久度"), SerializeField]
+	protected int breakHp;        //	耐久度(斬る・投げるアイテムは回数)
+
+	[Header("ダメージ数"), SerializeField]
+	protected float attackPoint;  //	攻撃力・威力
+
+	[Header("回復量"), SerializeField]
+	protected uint healPoint;     //	回復量
+
+	[Header("ID"), SerializeField]
+	protected ItemManager.ItemID id;	//	アイテムの識別ID
+
+
+	//	状態のフラグ管理群
+	protected bool isHave = false;      //	拾われているか(落ちているか、所持しているか)
+	protected bool isUse = false;       //	使っているか(武器・防具として使用しているか)
+	public bool IsActiveItem { get { return (isHave && isUse); } }	//	持っていて、使用している時のみtrueを返却
+
 
 	//	プロパティ
-	public float BreakHP{get;}
-	public float AttackPoint{get;}
+	public int BreakHP { get { return breakHp; } }
+	public float AttackPoint { get { return attackPoint; } }
+	public uint HealPoint { get { return healPoint; } }
+	public ItemManager.ItemID ID { get { return id; } set { id = value; } }
 
 
 	//	メソッド
 	public abstract void Action();
 
-	public abstract void EatItem();
+	public abstract uint EatItem();
+
+	public void SubBreakHP(int value){
+		if(breakHp <= 0){
+			//TODO:	耐久度ゼロ以下の処理
+		}else{
+			breakHp -= value;   //	耐久度を減らす
+		}
+	}
+
 }
--- Program/Project/LoveProject/Assets/Users/Yamada/ItemYMD/Shoot.cs
+++ Program/Project/LoveProject/Assets/Users/Yamada/ItemYMD/Shoot.cs
@@ -2,59 +2,97 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//	空のオブジェクトにコンポーネント？
+//	弾の親オブジェクトクラス
 public class Shoot : Item{
+
+	#region Member
 
 	/*[SerializeField]*/public Vector3 target{get; set;}	//	飛んでいく対象座標
 
+	//	親が弾オブジェクトを生成するため
+	[NamedArrayAttribute(new string[] { "マーブルチョコ", "飴玉" })]
+	public GameObject[] bulletPrefab = new GameObject[2];	//	弾Prefabの格納テーブル
+	int createObjNum = 0;	//	生成するPrefabの番号
+
+	//	弾オブジェクトの格納リスト
+	List<GameObject> bulletList = new List<GameObject>();
+	//	現在のリストの位置
+	int currentBulletList = 0;
+
+	//	マーブルチョコの弾数固定値
+	const int ChocoBulletNum = 4;
+	//	飴玉の弾数固定値
+	const int CandyBulletNum = 2;
+	//	弾数の最大（可変）
+	int MaxBullet = 0;
 	
-	[SerializeField]bool isShoot = false;	//	投げられた(発射された)
-	[SerializeField]float testSpeed = 2;	//hoge
+	#endregion Member
 
+	#region Method
 
+	void Start(){
+		//	必要な弾数を生成
+		//	生成する弾の数を決める。　マーブルは４、飴玉は２
+		if(this.ID == ItemManager.ItemID.MarbleChoco){
+			MaxBullet = ChocoBulletNum;
+			createObjNum = 0;
+
+		}else if(this.ID == ItemManager.ItemID.Candy){
+			MaxBullet = CandyBulletNum;
+			createObjNum = 1;
+		}
+		//	弾生成ループ
+		for(int cnt = 0; cnt < MaxBullet; cnt++){
+			//	作って、子にして、表示オフ
+			bulletList.Add((GameObject)Instantiate(bulletPrefab[(int)createObjNum], transform.position, Quaternion.identity));
+			bulletList[cnt].transform.parent = transform;
+			bulletList[cnt].SetActive(false);
+		}
+		bulletList[currentBulletList].SetActive(true);	//	落ちているときに実体が必要
+	}
 
 	void Update(){
 		
 		//	テスト動作
 		{
-			if(isShoot){//	とりあえずフラグでif
-				Shooting();
-			}else{
-				var angle = transform.eulerAngles;
-				angle.y = 90.0f * Mathf.Sin(Time.time);
-				transform.eulerAngles = angle;
-				transform.Rotate(transform.eulerAngles);
-			}
+			//処理
 		}
 	}
 
-
-
+	/// <summary>
+	/// 
+	/// </sammary>
 	public override void Action(){
-		isShoot = true;
-		transform.LookAt(target);	//	対象へ向く(Z軸を合わせる)
+		//	子の弾の行動開始呼び出し
+		//	使用可能弾数オーバーチェック
+		if(currentBulletList >= MaxBullet) return;
+		
+		//	使用する弾を、アクティブ化
+		bulletList[currentBulletList].SetActive(true);
+		//	弾の行動開始
+		bulletList[currentBulletList].GetComponent<Bullet>().ActionBullet();
+		CurrentUp();
+		
+		isUse = true;
 	}
 
 
-	
-	public override void EatItem(){
-		//TODO:	食べられた時の処理
+	/// <summary>
+	/// 食べられた時の処理
+	/// </sammary>
+	public override uint EatItem(){
+		//	回復量を返す。
+		return HealPoint;
 	}
 
 
-
-	//	直進するだけ～
-	private void Shooting(){
-		//	Z方向へ直進
-		transform.position += transform.forward * testSpeed * Time.deltaTime;
+	/// <summary>
+	/// 使用する弾のリストの、現在の位置を進める
+	/// </sammary>
+	private void CurrentUp(){
+		currentBulletList += 1;
 	}
-	
 
-
-	//	衝突検知
-	void OnTriggerEnter(Collider other){
-
-		//TODO:	とりあえず当たったかをテスト。
-		Debug.Log(this.name + "は" + other.gameObject.name + "と当たった！");
-	}
-	//TODO:　衝突時の自壊
+	#endregion Method
 }
