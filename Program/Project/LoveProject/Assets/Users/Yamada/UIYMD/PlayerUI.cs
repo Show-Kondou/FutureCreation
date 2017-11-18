cde13a18291f755e4d193ea2ce5c9f35674ef731
Issei <hal.nagoya40077@gmail.com>
2017-11-19 01:59:09 +0900

    UIをプレイヤー情報取得可能状態にした

--- Program/Project/LoveProject/Assets/Users/Yamada/UIYMD/PlayerUI.cs
+++ Program/Project/LoveProject/Assets/Users/Yamada/UIYMD/PlayerUI.cs
@@ -7,24 +7,80 @@ public class PlayerUI : MonoBehaviour {
 	[Header("プレイヤー"),SerializeField]
 	private Player _player;
 
-	uint playerID;
-	int hp;
-	ItemManager.ItemType type;
+	ItemsUI itemsUIL;	//	アイテムUI左
+	ItemsUI itemsUIR;	//	アイテムUI右
+	HPUI	hpUI;		//	HPUI
 
 	// Use this for initialization
 	void Start () {
-		
+		itemsUIL = transform.Find("UI_Item_L").gameObject.GetComponent<ItemsUI>();
+		itemsUIR = transform.Find("UI_Item_R").gameObject.GetComponent<ItemsUI>();
+		hpUI = transform.Find("UI_Gauge").gameObject.GetComponent<HPUI>();
+
+		//	アイテムUIへアイテム情報を投げ
+		//itemsUIL.SetItemUI(_player.ItemTypeL);
+		//itemsUIR.SetItemUI(_player.ItemTypeR);
+
+		//	HPUIへHPを投げ
+		//hpUI.playerHp = _player.PlayerHP;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		//	アイテムUIへアイテム情報を投げ
+		//itemsUIL.SetItemUI(_player.ItemTypeL);
+		//itemsUIR.SetItemUI(_player.ItemTypeR);
+
+		//	HPUIへHPを投げ
+		//hpUI.playerHp = _player.PlayerHP;
 		
+		//	ボタンUI4更新
+		// if(InputGame.GetPlayerItemL()){
+		// 	itemsUIL.PushedButton(1);
+		// }
+		// if(InputGame.GetPlayerItemR()){
+		// 	itemsUIR.PushedButton(1);
+		// }
+		// if(InputGame.GetPlayerEatL()){
+		// 	itemsUIL.PushedButton(2);
+		// }
+		// if(InputGame.GetPlayerEatR()){
+		// 	itemsUIR.PushedButton(2);
+		// }
+
+
+		DebugButtonUI();
+	}
+
+
+	void DebugButtonUI(){
+		//	ボタンUI4更新
+		if(Input.GetKey(KeyCode.V)){
+			itemsUIL.PushedButton(1);
+		}else if(Input.GetKey(KeyCode.B)){
+			itemsUIR.PushedButton(1);
+		}else if(Input.GetKey(KeyCode.N)){
+			itemsUIL.PushedButton(2);
+		}else if(Input.GetKey(KeyCode.M)){
+			itemsUIR.PushedButton(2);
+		}
+
+		if(Input.GetKeyUp(KeyCode.V)){
+			itemsUIL.ReleaseButton(1);
+		}
+		if(Input.GetKeyUp(KeyCode.B)){
+			itemsUIR.ReleaseButton(1);
+		}
+		if(Input.GetKeyUp(KeyCode.N)){
+			itemsUIL.ReleaseButton(2);
+		}
+		if(Input.GetKeyUp(KeyCode.M)){
+			itemsUIR.ReleaseButton(2);
+		}
 	}
 
 	
-	void OnTriggerEnter(Collider other){
-		
-	}
 
 
 }
