3e1efb94fe59e3821c8cb165a0d857bdb010c1bd
Issei <hal.nagoya40077@gmail.com>
2017-11-18 10:53:19 +0900

    UI関係。タイマー・HP表示はほぼ完。

---
+++ Program/Project/LoveProject/Assets/Users/Yamada/UIYMD/GameTimer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour {
+
+	//	メンバ
+	private float timeLimit = 180;	//	秒
+
+	//	アクセサ
+	public float TimeLimit{get{return timeLimit;}}
+	public int Minute{ get{ return (int) (timeLimit / 60.0f); } }
+	public int Second{ get{ return (int)(timeLimit % 60.0f); } }
+
+	//	シングルトン
+	#region Singleton
+        static GameTimer instance;
+        public static GameTimer Instance{
+            get{
+                if (!instance){
+                    instance = FindObjectOfType<GameTimer>();
+                    if (!instance)  instance = new GameObject("GameTimer").AddComponent<GameTimer>();
+                }
+                return instance;
+            }
+        }
+	#endregion Singleton
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//	一応残しておいて
+		// int m =  (int) (timeLimit / 60.0f);
+		// int s = (int)(timeLimit % 60.0f);
+		// Debug.Log( m + ":" + s.ToString("00") );
+
+		if(timeLimit < 0) return;
+
+		timeLimit -= Time.deltaTime;	//	秒減らし
+
+	}
+
+
+}
--- Program/Project/LoveProject/Assets/Users/Yamada/UIYMD/TimerUI.cs
+++ Program/Project/LoveProject/Assets/Users/Yamada/UIYMD/TimerUI.cs
@@ -1,21 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerUI : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	[SerializeField]private Sprite[] spriteNumbers = new Sprite[10];//0~9で10個
+	int prevSecond = 0;	//	前フレームの時間
+
+	private List<Image> numberImage = new List<Image>();
+
+	void Start(){
+		//	子のImageを取得
+		foreach(Transform child in transform){
+			numberImage.Add(child.GetComponent<Image>());
+		}
+
+		//	スプライトの初期設定
+		numberImage[0].sprite = spriteNumbers[0];
+		numberImage[1].sprite = spriteNumbers[0];
+		numberImage[2].sprite = spriteNumbers[3];
 	}
 
-	
-	void OnTriggerEnter(Collider other){
+	void Update(){
+
+		int seconds = GameTimer.Instance.Second;
+
+		//	前と違うときだけ更新
+		if(prevSecond == seconds) return;
+		prevSecond = seconds;	//	タイマー保存
+
+		//	スプライト割り当て
+		for(int index = 0; index < 2; index ++){
+			numberImage[index].sprite = spriteNumbers[seconds % 10];
+			seconds /= 10;
+		}
+		numberImage[2].sprite = spriteNumbers[GameTimer.Instance.Minute];
 		
 	}
 
