bb46257de96503083a9030eef5c7da4cff9a76e7
Issei <hal.nagoya40077@gmail.com>
2017-12-01 21:10:32 +0900

    タイトル作り始めました_Yamada

---
+++ Program/Project/LoveProject/Assets/Users/Yamada/TitleYMD/Script/Title.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// タイトル全体的なことする
+/// </sammary>
+public class Title : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+		//	入力待ち
+		WaitInput();
+	}
+
+
+
+	/// <summary>
+	/// タイトルシーン内での入力待ち処理記述
+	/// </sammary>
+	private void WaitInput(){
+		//TODO:	おそらくシーンマネージャーの遷移開始的なのを呼ぶ
+	}
+
+
+}
