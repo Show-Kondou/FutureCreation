de536595a7ce301ae78a670440e031c288c373e3
PC\k1z0n_000 <k1z0n1r7@yahoo.co.jp>
2017-12-06 11:17:23 +0900

    Popのエラー解消_Yamada

---
+++ Program/Project/LoveProject/Assets/Game/Script/Stage/StagePop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StagePop : MonoBehaviour {
+
+	[Header("こいつがお菓子を生成できる確率")]
+	public uint popPriority;
+
+
+	void Awake(){
+		//	自身でマネージャのリストに飛び込む
+		StageManager.Instance.popPoint.Add(this);
+
+#if UNITY_EDITOR	//	エディター実行時は見やすさのために、マネージャの子にする。
+		transform.parent = StageManager.Instance.transform;
+#endif
+
+	}
+
+	/// <summary>
+	///	指定した種類のお菓子をマネージャで生成
+	/// </sammary>
+	public void Pop(ItemManager.ItemType type){
+		ItemManager.Instance.Pop( type, transform.position);
+	}
+	
+
+
+}
