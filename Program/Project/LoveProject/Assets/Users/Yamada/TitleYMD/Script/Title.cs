9bd88168d327c03488cbaefce27bdc27130bbe01
Issei_desk <hal.nagoya40077@gmail.com>
2017-12-07 03:03:19 +0900

    タイトルの動き・シェーダー追加_Yamada

--- Program/Project/LoveProject/Assets/Users/Yamada/TitleYMD/Script/Title.cs
+++ Program/Project/LoveProject/Assets/Users/Yamada/TitleYMD/Script/Title.cs
@@ -6,42 +6,86 @@ using UnityEngine;
 /// <summary>
 /// タイトル全体的なことする
 /// </sammary>
-public class Title : MonoBehaviour {
+public class Title : MonoBehaviour
+{
 
-	[SerializeField]
-	Camera camera;
+    [SerializeField]
+    GameObject rendererCamera;
 
-	Animator cameraAnim;
+    [HideInInspector]
+    public Animator cameraAnim;
 
-	// Use this for initialization
-	void Start () {
-		//	カメラのアニメーターを取得
-		cameraAnim = camera.GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public bool IsGauss = false;
+    public Gauss gauss = null;
+    public float intencity = 0;
 
+    // Use this for initialization
+    void Start()
+    {
+        //	カメラのアニメーターを取得
+        cameraAnim = rendererCamera.GetComponent<Animator>();
+        cameraAnim.speed = 0;
+    }
 
-		
-		//	入力待ち
-		WaitInput();
-	}
+    // Update is called once per frame
+    void Update()
+    {
 
+        //if (IsGauss)
+        //    intencity += Time.deltaTime * 8;
+        //else
+        //    intencity -= Time.deltaTime * 8;
 
-
-	/// <summary>
-	/// タイトルシーン内での入力待ち処理記述
-	/// </sammary>
-	private void WaitInput(){
-		//TODO:	おそらくシーンマネージャーの遷移開始的なのを呼ぶ
-	}
-
-
-	private void PlayCameraAnim(){
-		//TODO:	カメラの移動アニメーション開始呼び出し
-		//cameraAnim.Play();
-	}
+        intencity = Mathf.Clamp01(intencity);
+        gauss.Resolution = (int)(intencity * 10);
 
 
+
+        //	入力待ち
+        WaitInput();
+    }
+
+
+
+    /// <summary>
+    /// タイトルシーン内での入力待ち処理記述
+    /// </sammary>
+    private void WaitInput()
+    {
+        //TODO:	おそらくシーンマネージャーの遷移開始的なのを呼ぶ
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayCameraAnim();
+        }
+    }
+
+
+    private void PlayCameraAnim()
+    {
+        //TODO:	カメラの移動アニメーション開始呼び出し
+        cameraAnim.speed = 1;
+        //cameraAnim.Play("CameraTilt", 0, 0.0f);
+    }
+
+
+    static Title instance;
+    public static Title Instance
+    {
+        get
+        {
+            if (!instance)
+            {
+                instance = FindObjectOfType<Title>();
+                if (!instance) instance = new GameObject("Title").AddComponent<Title>();
+            }
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (Instance && instance != this)
+            Destroy(gameObject);
+        
+    }
 }
