9f234e4f0fe9a0b98f91665a6008779cb05acf64
PC\k1z0n_000 <k1z0n1r7@yahoo.co.jp>
2017-12-06 17:05:56 +0900

    アイテムUIのヌルリファ解消_Yamada

--- Program/Project/LoveProject/Assets/Users/Yamada/TitleYMD/Script/Title.cs
+++ Program/Project/LoveProject/Assets/Users/Yamada/TitleYMD/Script/Title.cs
@@ -8,9 +8,15 @@ using UnityEngine;
 /// </sammary>
 public class Title : MonoBehaviour {
 
+	[SerializeField]
+	Camera camera;
+
+	Animator cameraAnim;
+
 	// Use this for initialization
 	void Start () {
-		
+		//	カメラのアニメーターを取得
+		cameraAnim = camera.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -29,6 +35,12 @@ public class Title : MonoBehaviour {
 	/// </sammary>
 	private void WaitInput(){
 		//TODO:	おそらくシーンマネージャーの遷移開始的なのを呼ぶ
+	}
+
+
+	private void PlayCameraAnim(){
+		//TODO:	カメラの移動アニメーション開始呼び出し
+		cameraAnim.Play();
 	}
 
 
