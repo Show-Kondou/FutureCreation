8a4b6ad9ad4edd190c6060d1874899950ab91cfe
PC\k1z0n_000 <k1z0n1r7@yahoo.co.jp>
2017-12-10 15:32:10 +0900

    PC移行_Yamada

---
+++ Program/Project/LoveProject/Assets/Users/Yamada/Hoge/GaugeTest/MaskGauge.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MaskGauge : MonoBehaviour {
+
+	RawImage rawImage;
+	
+
+	// Use this for initialization
+	void Start () {
+		rawImage = GetComponent<RawImage>();
+		rawImage.uvRect = new Rect(0,4,1,1);
+		Debug.Log(rawImage.uvRect.height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
