a92f43f234f6d4c6554c793ac80c5a76889ad738
Show Koudou <mail.show.Kondou@gmail.com>
2018-01-23 16:57:18 +0900

    リザルトプレイヤー作成_Show

--- Program/Project/LoveProject/Assets/Game/Script/Player/PlayerMove.cs
+++ Program/Project/LoveProject/Assets/Game/Script/Player/PlayerMove.cs
@@ -82,12 +82,10 @@ public class PlayerMove : PlayerBase {
 		if( _InputMove.magnitude == 0.0F ) {
 			Status.State = PlayerStatus.STATE.STAND;
 			Status.LowerState = PlayerStatus.STATE.STAND;
-			Debug.Log("Stand");
 		}else {
 			Status.State = PlayerStatus.STATE.RUN;
 			Status.LowerState = PlayerStatus.STATE.RUN;
 			ParticleManager.Instance.PlayParticle(ParticleManager.ParticleName.Smoke,transform.position );
-			Debug.Log( "Run" );
 
 		}
 	}
